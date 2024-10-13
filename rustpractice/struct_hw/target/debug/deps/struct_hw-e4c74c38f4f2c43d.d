d30c2cd12ae8191731262d571398caab2c3633de
Daamuzu1 <darrylamuzu@gmail.com>
2024-10-13 23:05:41 +0000

    Rust Struct Homework

---
+++ rustpractice/struct_hw/target/debug/deps/struct_hw-e4c74c38f4f2c43d.d
@@ -0,0 +1,5 @@
+/workspaces/OSFALL_2024/rustpractice/struct_hw/target/debug/deps/struct_hw-e4c74c38f4f2c43d: src/main.rs
+
+/workspaces/OSFALL_2024/rustpractice/struct_hw/target/debug/deps/struct_hw-e4c74c38f4f2c43d.d: src/main.rs
+
+src/main.rs:
---
+++ rustpractice/struct_hw/target/debug/struct_hw.d
@@ -0,0 +1,1 @@
+/workspaces/OSFALL_2024/rustpractice/struct_hw/target/debug/struct_hw: /workspaces/OSFALL_2024/rustpractice/struct_hw/src/main.rs
